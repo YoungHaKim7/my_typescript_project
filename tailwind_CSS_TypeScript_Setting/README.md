d5cb4c6c9a765fe3f467d41d0208c9e5d43a2873
YoungHa Kim <ytok1108@aol.com>
2023-07-16 17:20:41 +0900

    typescript & tailwind

---
+++ tailwind_CSS_TypeScript_Setting/README.md
@@ -0,0 +1,7 @@
+# Tailwind CSS TypeScript 
+
+https://flowbite.com/docs/getting-started/typescript/
+
+# TypeScript, Tailwind CSS를 Webpack 으로 말기 
+
+https://velog.io/@milmilkim/typescript-tailwind-css%EB%A5%BC-webpack%EC%9C%BC%EB%A1%9C-%EB%A7%90%EA%B8%B0
---
+++ tailwind_CSS_TypeScript_Setting/getting_Stated01/dist/app-bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+var text = "Hello TypeScript";
+console.log(text);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsSUFBTSxJQUFJLEdBQVcsa0JBQWtCLENBQUM7QUFDeEMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxJQUFJLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2dldHRpbmdfc3RhdGVkMDEvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdGV4dDogc3RyaW5nID0gXCJIZWxsbyBUeXBlU2NyaXB0XCI7XG5jb25zb2xlLmxvZyh0ZXh0KTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
