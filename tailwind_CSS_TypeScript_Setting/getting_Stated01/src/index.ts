30f78f6b9cc7f097df72c70e25e9e1d6b2aa82d8
YoungHa Kim <ytok1108@aol.com>
2023-07-16 17:33:10 +0900

    typescript & tailwind

--- tailwind_CSS_TypeScript_Setting/getting_Stated01/src/index.ts
+++ tailwind_CSS_TypeScript_Setting/getting_Stated01/src/index.ts
@@ -1,2 +1,11 @@
-const text: string = "Hello TypeScript";
-console.log(text);
+import { Modal } from 'flowbite'
+
+// select the two elements that we'll work with
+const $buttonElement: HTMLElement = document.querySelector('#button');
+const $modalElement: HTMLElement = document.querySelector('#modal');
+
+// create a new modal component
+const modal = new Modal($modalElement);
+
+// toggle the visibility of the modal when clicking on the button
+$buttonElement.addEventListener('click', () => modal.toggle());
